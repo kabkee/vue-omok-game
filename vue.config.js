fe6be479f739f296741a27271c36eb88ee8d5fa2
Kabkee Moon <kabkee@live.com>
2020-02-05 00:02:10 +0800

    build 에서만 /vue-omok-game 의 publicPath가 적용되도록 수정

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,7 @@ module.exports = {
 	"transpileDependencies": [
 		"vuetify"
 	],
-	publicPath: '/vue-omok-game'
+	publicPath: process.env.NODE_ENV === 'production'
+    ? '/vue-omok-game'
+    : '/'
 }
